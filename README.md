21278d74a63a62b131c77fbb743e59e34ee11ad5
Wesley Watkins <the_wesley_watkins@yahoo.com>
2019-12-30 12:46:12 -0500

    Added numbers to indicate values in Q-table

--- README.md
+++ README.md
@@ -3,6 +3,18 @@ This is an example of q-learning
 
 # Libraries Used
 - numpy
+- pandas
+- pygame
 
 # How To Run
 - run the command 'python3 game.py'
+
+# How It Works
+- The object of the game is for the blue circle to make its way to the yellow circle
+- The red circles indicate barriers that the blue circle is not allowed to run into; if it does, it must restart
+- The numbers indicate the current max value in the Q-Table for that state
+
+# Tips
+- To slow down the learning, simply pass a slower FPS value into the initialization of the 'game' variable (i.e. game = Game(30))
+- You can stop the program and it will write the values in the Q-table to a file called qtable.npy and will reuse this file when the program starts again
+- To complete reset learning, delete the qtable.npy file
--- game.py
+++ game.py
@@ -17,13 +17,14 @@ pygame.font.init()
 
 class Game:
 
-    def __init__(self):
+    def __init__(self, max_fps=0):
         # display setup
         self.clock = pygame.time.Clock()
+        self.max_fps = max_fps
         pygame.display.set_caption("AI World")
         self.screen = pygame.display.set_mode((globals.WIDTH, globals.HEIGHT))
         # game setup
-        self.myfont = pygame.font.SysFont('Comic Sans MS', 30)
+        self.myfont = pygame.font.SysFont('Comic Sans MS', 18)
         self.playing = True
         # setup game board
         self.rows = 10
@@ -47,6 +48,8 @@ class Game:
         self.player = Player(0, 0)
         # setup ai
         self.wesley = AI(self.player, self.game_table, self.rows, self.cols)
+        # add fonts
+
 
     def process_events(self):
         for event in pygame.event.get():
@@ -72,10 +75,20 @@ class Game:
                                        int(size / 3))
                 if self.game_table[i, j] == 2:
                     pygame.draw.circle(self.screen, globals.RED, (int(size/2 + size * j), int(size/2 + size * i)), int(size/3))
+        pygame.draw.circle(self.screen, globals.YELLOW, (int(size * self.rows - size/2), int(size * self.cols - size/2)), int(size/3))
+        state = 0
+        for i in range(self.rows):
+            for j in range(self.cols):
+                s = self.myfont.render(str(int(self.wesley.max_q(state))), True, globals.BLACK)
+                s_rect = s.get_rect()
+                size_x = s_rect.w
+                size_y = s_rect.h
+                self.screen.blit(s, (int(size / 2 + size * j - size_x/2), int(size / 2 + size * i - size_y/2)))
+                state += 1
         pygame.display.flip()
 
     def run(self):
-        dt = self.clock.tick(10)
+        dt = self.clock.tick(self.max_fps)
         self.process_events()
         self.update()
         self.render()
